b37b113c0deeda84275f62be35e82042c2a06517
trevordino <trevor.ng.2018@sis.smu.edu.sg>
2020-09-04 23:34:09 +0800

    b

--- Microservices/GlossaryService/GlossaryService/urls.py
+++ Microservices/GlossaryService/GlossaryService/urls.py
@@ -1,4 +1,4 @@
-"""GlossaryAPI URL Configuration
+"""GlossaryService URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
