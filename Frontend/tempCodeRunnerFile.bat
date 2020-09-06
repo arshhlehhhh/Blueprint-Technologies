5ce765956e9f38936469b904249a237e522bba71
kinhou <47879876+kinhou@users.noreply.github.com>
2020-09-06 20:54:01 +0800

    Completed Article

---
+++ Frontend/tempCodeRunnerFile.bat
@@ -0,0 +1,6 @@
+
+START ..\Microservices\InsightService\manage.py
+START ..\Microservices\GlossaryService\manage.py
+START ..\Microservices\DemoGame\DemoGame.py
+START ..\Microservices\AccountDemo\AccountDemo.py
+START ..\Microservices\Account\Account.py
